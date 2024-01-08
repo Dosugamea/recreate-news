a23e5828f59426c83699ca883d8e15555060a53f
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-08 21:34:28 +0900

    feat(page): add detail page template

---
+++ src/pages/detail.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+useServerHead({
+  titleTemplate: 'detail'
+})
+</script>
+
+<template>
+  <v-row>
+    <v-col cols="12">
+      <deco-chip type="news" class="mb-1" />
+      <deco-chip type="update" class="mb-1" />
+      <deco-chip type="maintenance" class="mb-1" />
+      <news-post />
+    </v-col>
+    <v-col cols="12">
+      <p>『』をご利用いただき、誠にありがとうございます。</p>
+      <br />
+      <p>アップデートにおいて実装予定の機能改善内容をご案内いたします。</p>
+      <br />
+    </v-col>
+    <v-col cols="12">
+      <v-img
+        :aspect-ratio="3 / 1"
+        src="https://placehold.jp/720x480.png?text=thumbnail"
+      />
+    </v-col>
+    <v-col cols="12">
+      <deco-section text="開催期間" />
+      <p>1月8日 17:00 ～ 2月4日 16:59</p>
+    </v-col>
+  </v-row>
+</template>
