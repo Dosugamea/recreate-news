8af215d24dea7bbd3a0767b5c59d2f2ec90ba6ae
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-08 21:33:00 +0900

    refactor(deco-section): remove unused class

--- src/components/DecoSection.vue
+++ src/components/DecoSection.vue
@@ -11,7 +11,6 @@ const props = withDefaults(defineProps<Props>(), {
   <div>
     <div class="d-flex align-center">
       <v-img
-        :contain="false"
         class="flex-grow-0"
         src="/deco-hitode.png"
         width="24"
@@ -19,16 +18,6 @@ const props = withDefaults(defineProps<Props>(), {
       />
       <span class="ml-1 flex-grow-1 text-info" v-text="props.text" />
     </div>
-    <v-divider
-      class="my-1 border-opacity-100 gradient-divider"
-      color="info"
-      :thickness="3"
-    />
+    <v-divider class="my-1 border-opacity-100" color="info" :thickness="3" />
   </div>
 </template>
-
-<style scoped lang="scss">
-.gradient-divider {
-  background-image: linear-gradient(90deg, transparent 0% 0%, #fff 70% 100%);
-}
-</style>
