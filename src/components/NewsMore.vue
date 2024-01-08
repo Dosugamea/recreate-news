78177fd92a9cd8c73edd31523accdc7de8b74c54
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-08 20:04:06 +0900

    feat(news-more): implement news-more

---
+++ src/components/NewsMore.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Emits {
+  (event: 'click'): void
+}
+const emits = defineEmits<Emits>()
+</script>
+
+<template>
+  <v-btn
+    class="border-strong w-100 ma-1 bg-news-base text-primary"
+    size="large"
+    elevation="0"
+    @click="emits('click')"
+  >
+    <div class="mx-1 my-2">
+      <span class="font-weight-bold">▼もっと表示▼</span>
+    </div>
+  </v-btn>
+</template>
+
+<style scoped lang="scss">
+.border-strong {
+  outline: 3px solid rgb(var(--v-theme-news-wrap));
+  outline-offset: -3px;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,6 +10,9 @@ useServerHead({
       <v-col v-for="i in 12" :key="i" cols="12" class="ma-n1">
         <news-post />
       </v-col>
+      <v-col class="ma-n1">
+        <news-more />
+      </v-col>
     </v-row>
   </v-container>
 </template>
