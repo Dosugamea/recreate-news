8a4919333929ab7686af37db4bd2859073b56b5f
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-10 23:14:56 +0900

    fix(news-detail): fix invalid SSR result it works, but why it didn't?

--- src/components/NewsDetail.vue
+++ src/components/NewsDetail.vue
@@ -1,19 +1,3 @@
-<script setup lang="ts">
-interface Props {
-  endpoint: string
-  slug: string
-}
-const props = defineProps<Props>()
-
-const { locale } = useI18n()
-const { data } = await useAsyncData('article', async () => {
-  return await queryContent(
-    `/${locale.value}/${props.endpoint}/${props.slug}`
-  ).findOne()
-})
-</script>
-
 <template>
-  <ContentRenderer v-if="data" :value="data" />
-  <p v-else>Not found</p>
+  <ContentDoc />
 </template>
