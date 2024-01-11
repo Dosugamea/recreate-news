0f4cceebb1a75a04861b864a81f21103615cb69b
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-11 22:06:32 +0900

    feat(markdown): add utility class + section component

---
+++ src/components/content/PinkSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="mt-2 d-flex align-center">
+      <v-img
+        class="flex-grow-0"
+        src="/deco-hitode.png"
+        width="24"
+        height="24"
+      />
+      <span class="ml-1 flex-grow-1 text-info">
+        <slot />
+      </span>
+    </div>
+    <v-divider class="my-1 border-opacity-100" color="info" :thickness="3" />
+  </div>
+</template>
