b7fd17fbcd49faed4c5f607e33bc35228196abfe
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-11 21:06:18 +0900

    feat(news-detail): add header rendering support

--- src/components/NewsDetail.vue
+++ src/components/NewsDetail.vue
@@ -1,3 +1,16 @@
 <template>
-  <ContentDoc />
+  <ContentDoc v-slot="{ doc }">
+    <v-row justify="center">
+      <v-col cols="10" xs="12" sm="12" md="10" lg="8" xl="6">
+        <news-detail-chip class="mb-3" :type="doc.type" />
+        <news-list-post
+          class="mb-5"
+          :date="doc.date"
+          :title="doc.title"
+          :new="doc.new"
+        />
+        <ContentRenderer :value="doc" />
+      </v-col>
+    </v-row>
+  </ContentDoc>
 </template>
