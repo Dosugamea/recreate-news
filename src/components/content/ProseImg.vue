3ec861cb4a2332ec8634e993763d171651cf98bc
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-11 21:06:33 +0900

    feat(markdown): add v-img support

---
+++ src/components/content/ProseImg.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps({
+  src: {
+    type: String,
+    default: ''
+  },
+  alt: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: [String, Number],
+    default: undefined
+  },
+  height: {
+    type: [String, Number],
+    default: undefined
+  }
+})
+</script>
+
+<template>
+  <v-img :src="props.src" :alt="alt" :width="width" :height="height" />
+</template>
