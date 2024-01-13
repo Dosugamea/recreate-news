5ebdb8b7f5308e1a60f0954a6ced3fefa25f4cee
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-13 22:21:40 +0900

    feat(component-content): add divider as extra break lines

---
+++ src/components/content/ExtraDivider.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  height?: number
+}
+const props = withDefaults(defineProps<Props>(), {
+  height: 4
+})
+
+const topHeight = computed(() => {
+  return `mt-${props.height}`
+})
+</script>
+
+<template>
+  <v-divider class="border-opacity-0" :class="[topHeight]" />
+</template>
