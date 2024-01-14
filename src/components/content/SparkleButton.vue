54a9816d34b6ceb44860bc3443caeff229911cde
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-14 19:56:12 +0900

    feat(component-content): add sparkle-button

---
+++ src/components/content/SparkleButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { mdiStar } from '@mdi/js'
+
+interface Props {
+  href: string
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="d-flex justify-center align-center">
+    <v-btn
+      :prepend-icon="mdiStar"
+      :append-icon="mdiStar"
+      :href="props.href"
+      size="x-large"
+      elevation="6"
+      rounded="xl"
+      class="my-8 text-center text-white bg-orange"
+    >
+      <slot />
+    </v-btn>
+  </div>
+</template>
