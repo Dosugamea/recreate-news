63eee1846e32c8a4d1c74d77683837215ab77b2b
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-01-19 22:14:50 +0900

    refactor(components-content): add alt

--- src/components/content/SparkleSection.vue
+++ src/components/content/SparkleSection.vue
@@ -10,7 +10,13 @@ const imageSrc = useImageLink('/deco-hitode.png')
 <template>
   <div>
     <div class="mt-8 d-flex align-center">
-      <v-img class="flex-grow-0" :src="imageSrc" width="24" height="24" />
+      <v-img
+        class="flex-grow-0"
+        :src="imageSrc"
+        alt=""
+        width="24"
+        height="24"
+      />
       <span class="ml-1 flex-grow-1 text-info" v-text="props.title" />
     </div>
     <v-divider class="my-1 border-opacity-100" color="info" :thickness="3" />
